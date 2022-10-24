6293cf47de09ccd8fbf8a0bfcdd836db996a8e8e
Kevin Galligan <kgalligan@gmail.com>
2022-10-23 20:35:37 -0700

    Put podspec back

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -35,7 +35,7 @@ kmmbridge {
     mavenPublishArtifacts()
     githubReleaseVersions()
     spm()
-    // cocoapods("git@github.com:touchlab/PublicPodspecs.git")
+    cocoapods("git@github.com:touchlab/PublicPodspecs.git")
     versionPrefix.set("1.4")
 }
 
