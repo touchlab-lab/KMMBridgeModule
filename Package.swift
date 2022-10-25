03c75e5c3d1d1cfc9739a96072789dc7d5c86cfc
Kevin Galligan <kgalligan@gmail.com>
2022-10-24 21:40:55 -0700

    Local dev path in main

--- Package.swift
+++ Package.swift
@@ -1,8 +1,6 @@
 // swift-tools-version:5.3
 import PackageDescription
 
-let remoteKotlinUrl = "https://maven.pkg.github.com/touchlab-lab/KMMBridgeModule/KaMPKit/shared-kmmbridge/1.4.0/shared-kmmbridge-1.4.0.zip"
-let remoteKotlinChecksum = "120a115a937cbbf821aa463442a053cd66a5fec7a6fd7335ef2b4f9342d2a7b8"
 let packageName = "shared"
 
 let package = Package(
@@ -19,8 +17,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: packageName,
-            url: remoteKotlinUrl,
-            checksum: remoteKotlinChecksum
+            path: "./shared/build/XCFrameworks/debug/\(packageName).xcframework"
         )
         ,
     ]
